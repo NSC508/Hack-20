ee76447e9f6906cb216fca7d112fed6c32c10a75
Sandy Cheng <sandyc01@cs.washington.edu>
2020-08-16 10:03:26 -0700

    Multiple selectors added

--- app/src/App.js
+++ app/src/App.js
@@ -1,84 +1,110 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormInput from "./components/FormInput";
+import Selector from "./components/MajorSelector";
+import SelectMult from "./components/SelectMult";
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+      </Router>
+      <SelectMult />
+      <Selector />
+    </React.Fragment>
+  );
+}
+
+// async function getData(url = "http://127.0.0.1:5000/api/v1/getUser?email=sathvikchinta@gmail.com") {
+//   // Default options are marked with *
+//   const response = await fetch(url, {
+//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
+//     mode: 'no-cors', // no-cors, *cors, same-origin
+//     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     // credentials: 'same-origin', // include, *same-origin, omit
+//     // headers: {
+//     //   'Content-Type': 'application/json'
+//     //   // 'Content-Type': 'application/x-www-form-urlencoded',
+//     // },
+//     // redirect: 'follow', // manual, *follow, error
+//     // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     // body: JSON.stringify(data) // body data type must match "Content-Type" header
+//   });
+//   checkStatus(response);
+//   try {
+//     return response.json(); // parses JSON response into native JavaScript objects
+//   } catch(err) {
+//     console.error(err);
+//   }
+// }
+
+
+export default App;
+
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
 // import FormInput from "./components/FormInput";
 
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { NavigationBar } from './components/NavigationBar';
+// import { NavigationBar } from "./components/NavigationBar";
 
+// import { Home } from "./Home";
+// import { About } from "./About";
+// import { Submission } from "./Submission";
+// import Sidebar from "./components/Sidebar";
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <Router>
-//         <NavigationBar />
-//       </Router>
-//       <FormInput />
-//     </React.Fragment>
-//   );
+// import fire from "./components/config/fire";
+// import Login from "./components/Login";
+// import HomeTest from "./components/HomeTest";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       user: null,
+//     };
+//     this.authListener = this.authListener.bind(this);
+//   }
+//   componentDidMount() {
+//     this.authListener();
+//   }
+
+//   authListener() {
+//     fire.auth().onAuthStateChanged((user) => {
+//       if (user) {
+//         this.setState({ user });
+//       } else {
+//         this.setState({ user: null });
+//       }
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Router>
+//           <NavigationBar />
+
+//           <Sidebar />
+
+//           <Switch>
+//             <Route exact path="/" component={Home} />
+//             <Route path="/about" component={About} />
+//             <Route component={Submission} />
+//           </Switch>
+//         </Router>
+//         {this.state.user ? <HomeTest /> : <Login />}
+//       </React.Fragment>
+//     );
+//   }
 // }
 
 // export default App;
-
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import FormInput from "./components/FormInput";
-
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavigationBar } from "./components/NavigationBar";
-
-import { Home } from "./Home";
-import { About } from "./About";
-import { Submission } from "./Submission";
-import Sidebar from "./components/Sidebar";
-
-import fire from "./components/config/fire";
-import Login from "./components/Login";
-import HomeTest from "./components/HomeTest";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-    this.authListener = this.authListener.bind(this);
-  }
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
-      } else {
-        this.setState({ user: null });
-      }
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <NavigationBar />
-
-          <Sidebar />
-
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route component={Submission} />
-          </Switch>
-        </Router>
-        {this.state.user ? <HomeTest /> : <Login />}
-      </React.Fragment>
-    );
-  }
-}
-
-export default App;
--- app/src/components/FormInput.js
+++ app/src/components/FormInput.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik"; // must npm install formik to use it
+import { Formik, /* Field, Form,*/ ErrorMessage/*, FieldArray*/ } from "formik"; // must npm install formik to use it
 import * as Yup from "yup";
 import "../FormStyle.css";
+import Selector from "./MajorSelector"
 
 /**
  * Form component, used when users initially sign up for an account
  */
 
 function FormInput() {
+  const majorCollection = [
+    {label: "Applied and Computational Mathematical Sciences", value: "acms"},
+    {label: "Art: Interdisciplinary", value: "art_interdisciplinary"},
+    {label: "Art: Painting and Drawing", value: "art_paintingAndDrawing"},
+    {label: "Art: Photomedia", value: "art_photomedia"},
+    {label: "Art: Three Dimensional Forum", value: "art_threeDimensionalForum"},
+    {label: "Art History", value: "artHistory"},
+    {label: "Astronomy", value: "astronomy"},
+    {label: "Biochemistry", value: "biochemistry"},
+    {label: "Biology", value: "biology"},
+    {label: "Biology: Ecology, Evolution, and Conservation", value: "biology_ecology"},
+    {label: "Biology: Molecular, Cellular, and Development", value: "biology_molecular"},
+    {label: "Biology: Plant", value: "biology_plant"},
+    {label: "Computer Science", value: "computerScience"},
+    {label: "Dance", value: "dance"},
+    {label: "Drama", value: "drama"},
+    {label: "Mathematics", value: "math"},
+    {label: "Music", value: "music"},
+    {label: "Physics", value: "physics"},
+  ];
   return (
     <div>
-      <p>Hello world!</p>
+      <h3>Tell us a bit about yourself!</h3>
       <Formik
         initialValues={{
           firstName: "",
           lastName: "",
-          email: ""
+          email: "",
+          major: []
         }}
         validationSchema={Yup.object({
           firstName: Yup.string()
@@ -26,75 +48,129 @@ function FormInput() {
             .required("Required*"),
           email: Yup.string()
             .email("Must be a valid email")
+            .required("Required*"),
+          major: Yup.string()
             .required("Required*")
           })
         }
         onSubmit={(values, { setSubmitting } ) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
+            alert("this is working" + JSON.stringify(values, null, 2));
+            console.log("this is submitting");
             setSubmitting(false);
           }, 200);
         }}
-      >
-        <Form className="fullForm">
-          <label>Full Name
-            <Field
+        >
+      {(formik) => (
+        <form onSubmit={formik.handleSubmit} className="fillForm">
+          <label className="container">Full Name
+            <input
               name="firstName"
               type="text"
+              {...formik.getFieldProps("firstName")}
               placeholder="First Name"
             />
-            <div className="errorMsg"><ErrorMessage name="firstName" /></div>
-            <Field
+            {formik.touched.firstName && formik.errors.firstName ? (
+             <div>{formik.errors.firstName}</div>
+           ) : null}
+            {/* <div className="errorMsg"><ErrorMessage name="firstName" /></div> */}
+            <input
               name="lastName"
               type="text"
+              {...formik.getFieldProps("lastName")}
               placeholder="Last Name"
             />
-            <div className="errorMsg"><ErrorMessage name="lastName" /></div>
+            {formik.touched.firstName && formik.errors.firstName ? (
+             <div>{formik.errors.firstName}</div>
+           ) : null}
           </label>
-          <label>
-            <Field
+          <label className="container">
+            <input
               name="email"
               type="email"
+              {...formik.getFieldProps("email")}
               placeholder="xxx@example.com"
             />
+            {formik.touched.firstName && formik.errors.firstName ? (
+             <div>{formik.errors.firstName}</div>
+           ) : null}
           </label>
-          <div className="errorMsg"><ErrorMessage name="email" /></div>
           <button type="submit">Submit</button>
-        </Form>
+        </form>
+      )}
       </Formik>
-      {/*<form onSubmit={formik.handleSubmit}>
-        <label>Full Name
-          <input
-            name="firstName"
-            type="text"
-            {...formik.getFieldProps("firstName")}
-            placeholder="First Name"
-          />
-          {formik.touched.firstName && formik.errors.firstName ?
-            <div>{formik.errors.firstName}</div> : null}
-          <input
-            name="lastName"
-            type="text"
-            {...formik.getFieldProps("lastName")}
-            placeholder="Last Name"
-          />
-          {formik.touched.lastName && formik.errors.lastName ?
-            <div>{formik.errors.lastName}</div> : null}
-        </label>
-        <label>
-          <input
-            name="email"
-            type="email"
-            {...formik.getFieldProps("email")}
-            placeholder="xxx@example.com"
-          />
-        </label>
-        {formik.touched.email && formik.errors.email ?
-          <div>{formik.errors.email}</div> : null}
-        <button type="submit">Submit</button>
-        </form>*/}
     </div>
   )
 }
 
 export default FormInput;
+/*//   <Form className="fillForm">
+      //     <label className="container">Full Name:
+      //       <Field
+      //         name="firstName"
+      //         type="text"
+      //         placeholder="First Name"
+      //       />
+      //       <div className="errorMsg"><ErrorMessage name="firstName" /></div>
+      //       <Field
+      //         name="lastName"
+      //         type="text"
+      //         placeholder="Last Name"
+      //       />
+      //       <div className="errorMsg"><ErrorMessage name="lastName" /></div>
+      //     </label>
+      //     <label className="container"> Email:
+      //       <Field
+      //         name="email"
+      //         type="email"
+      //         placeholder="xxx@example.com"
+      //       />
+      //       <div className="errorMsg"><ErrorMessage name="email" /></div>
+      //     </label>
+      //     <label className="container"> Major:
+      //       <Field
+      //         name="major"
+      //         as="select"
+      //       >
+      //         <option value={null}>---Please Select a Major---</option>
+      //         <option value="astronomy">Astronomy</option>
+      //         <option value="biology">Biology</option>
+      //         <option value="computerScience">Computer Science</option>
+      //         <option value="computerScience">History</option>
+      //       </Field>
+      //       <div className="errorMsg"><ErrorMessage name="major" /></div>
+      //     </label>
+      //     <FieldArray
+      //       name="majors"
+      //       render={
+      //         (arrayHelpers) => (
+      //           <div>
+      //               {majorCollection.map(major => (
+      //                   <label key={major.value}>
+      //                       <input
+      //                           name="majors"
+      //                           type="checkbox"
+      //                           value={major}
+      //                           {...console.log(Formik.values)}
+      //                           checked={formik.values.majors.includes(major.value)}
+      //                           onChange={e => {
+      //                             if (e.target.checked) {
+      //                               arrayHelpers.push(major.value);
+      //                             } else {
+      //                               const idx = formik.values.majors.indexOf(major.value);
+      //                               arrayHelpers.remove(idx);
+      //                             }
+      //                           }}
+      //                       />
+      //                       <span>{major.label}</span>
+      //                   </label>
+      //               ))}
+      //           </div>
+      //         )
+      //       }
+      //     />
+      //    <Selector />
+      {
+      //     <button type="submit">Submit</button>
+      //   </Form>
+      /* </Formik> */
