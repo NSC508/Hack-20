9c3ad97a48dbc49fb06b46753c69f2278bb7ad56
williamvietnguyen <williamvnguyen2@gmail.com>
2020-08-16 10:33:28 -0700

    More styling

--- app/src/components/HomeTest.js
+++ app/src/components/HomeTest.js
@@ -8,9 +8,21 @@ class HomeTest extends React.Component {
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
         <h3>You are logged in! Head Over To Messaging!</h3>
-        <button onClick={this.logout}>Logout</button>
+        <button
+          style={{
+            margin: "10px",
+            background: "#5d8ffc",
+            color: "#fff",
+            border: "1px solid #5d8ffc",
+            borderRadius: "5px",
+            padding: "15px",
+          }}
+          onClick={this.logout}
+        >
+          Logout
+        </button>
       </div>
     );
   }
--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -32,7 +32,7 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
         <div>
           <div>Email</div>
           <input
