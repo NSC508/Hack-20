6476949271f4a4b2286b39c677d8b38b8f79fab3
williamvietnguyen <williamvnguyen2@gmail.com>
2020-08-16 00:22:39 -0700

    App.js is here.

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import FormInput from "./components/FormInput";
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavigationBar } from './components/NavigationBar';
+import { NavigationBar } from "./components/NavigationBar";
 
-import { Home } from './Home';
-import { About } from './About';
-import { Submission } from './Submission';
-import Sidebar from './components/Sidebar';
+import { Home } from "./Home";
+import { About } from "./About";
+import { Submission } from "./Submission";
+import Sidebar from "./components/Sidebar";
 
+import fire from "./components/config/fire";
+import Login from "./components/Login";
+import HomeTest from "./components/HomeTest";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Router>
-        <NavigationBar />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+    this.authListener = this.authListener.bind(this);
+  }
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
 
           <Sidebar />
 
-        <Switch>
-        <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={Submission} />
-        </Switch>
-      </Router>
-      <FormInput />
-    </React.Fragment>
-  );
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route component={Submission} />
+          </Switch>
+        </Router>
+        {this.state.user ? <HomeTest /> : <Login />}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
