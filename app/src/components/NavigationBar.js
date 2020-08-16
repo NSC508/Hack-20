8313e538ba0546858eaa882d9680dbc499d6f875
Kashish <kashish@cs.washington.edu>
2020-08-16 11:40:34 -0700

    Removed Matches

--- app/src/components/NavigationBar.js
+++ app/src/components/NavigationBar.js
@@ -21,7 +21,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/">FriendApp</Navbar.Brand>
+      <Navbar.Brand href="/">Mingle</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Form className="form-center">
         <FormControl type="text" placeholder="Search" className="" />
